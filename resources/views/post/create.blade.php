a2dfcfc4498a5a8af48e8cdc14dd2dbab8fd3c06
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2022-01-15 21:54:19 +0700

    upd

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,12 +1,18 @@
-<form action="{{ route('post.store') }}" method="post">
-    @csrf
-<div>
-    <label for="">Title</label>
-    <input type="text" name="title">
+@extends('layouts.public')
+
+@section('content')
+<div class="container my-4">
+    <form action="{{ route('post.store') }}" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="">Title</label>
+            <input type="text" name="title" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="">Body</label>
+            <textarea name="body" id="" cols="30" rows="10" class="form-control"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Save</button>
+    </form>
 </div>
-<div>
-    <label for="">Body</label>
-    <textarea name="body" id="" cols="30" rows="10"></textarea>
-</div>
-<button type="submit">Save</button>
-</form>
+@endsection
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -1,13 +1,19 @@
-<form action="{{ route('post.update', $post->id) }}" method="post">
-    @csrf
-    @method('PATCH')
-<div>
-    <label for="">Title</label>
-    <input type="text" name="title" value="{{ $post->title }}">
+@extends('layouts.public')
+
+@section('content')
+<div class="container my-4">
+    <form action="{{ route('post.update', $post->id) }}" method="post">
+        @csrf
+        @method('PATCH')
+        <div class="form-group">
+            <label for="">Title</label>
+            <input type="text" name="title" value="{{ $post->title }}" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="">Body</label>
+            <textarea name="body" id="" cols="30" rows="10" class="form-control">{{ $post->body }}</textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Save</button>
+    </form>
 </div>
-<div>
-    <label for="">Body</label>
-    <textarea name="body" id="" cols="30" rows="10">{{ $post->body }}</textarea>
-</div>
-<button type="submit">Save</button>
-</form>
+@endsection
